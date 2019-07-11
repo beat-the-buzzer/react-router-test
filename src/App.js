ff2455722d92e5efcd657c63b3c9300bd7280132
beat-the-buzzer <2985593303@qq.com>
2019-07-11 20:42:36 +0800

    format code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import {Link,HashRouter,Route} from 'react-router-dom';
+import { Link, HashRouter, Route } from 'react-router-dom';
 
 import Menu1 from './menu1/view'
 import Menu2 from './menu2/view'
 import Menu3 from './menu3/view'
+import Menu4 from './menu4/view'
 
-const Home = ()=><h2>这是首页</h2>
+const Home = () => <h2>这是首页</h2>
 
 class App extends Component {
   render() {
@@ -14,21 +15,22 @@ class App extends Component {
     return (
       <HashRouter>
         <div id="wrapper">
+          <ul>
+            <li><Link to="/">首页</Link></li>
+            <li><Link to="/menu1">基本应用</Link></li>
+            <li><Link to={`/menu2/${time}`}>url带参数</Link></li>
+            <li><Link to={{ pathname: "/menu3", data: { name: "hello", value: "world" } }}>非url带参数</Link></li>
+            <li><Link to="/menu4">新增的例子</Link></li>
+          </ul>
 
-            <ul>
-                <li><Link to="/">首页</Link></li>
-                <li><Link to="/menu1">基本应用</Link></li>
-                <li><Link to={`/menu2/${time}`}>url带参数</Link></li>
-                <li><Link to={{pathname:"/menu3",data:{name:"hello",value:"world"}}}>非url带参数</Link></li>
-            </ul>
-
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/menu1" component={Menu1}/>
-            <Route exact path="/menu2/:pid" component={Menu2}/>
-            <Route exact path="/menu3" component={Menu3}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/menu1" component={Menu1} />
+          <Route exact path="/menu2/:pid" component={Menu2} />
+          <Route exact path="/menu3" component={Menu3} />
+          <Route exact path="/menu4" component={Menu4} />
 
         </div>
-    </HashRouter>
+      </HashRouter>
     );
   }
 }
--- src/menu1/view.js
+++ src/menu1/view.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
-class Menu1 extends Component{
-    render(){
-        console.log(this.props)
-        return (
-            <div>
-                <h3>这是router的基本用法！</h3>
-            </div>
-        )
-    }
+class Menu1 extends Component {
+	render() {
+		console.log(this.props.location);
+		if(this.props.location.param && this.props.location.param.text) {
+			alert(this.props.location.param.text)
+		}
+		return (
+			<div>
+				<h3>这是router的基本用法！</h3>
+			</div>
+		)
+	}
 }
 
 export default Menu1;
